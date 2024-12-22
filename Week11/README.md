47e8cad1e4cc70962a0ec31d9d1e0f94e3770cdf
Poseidon <armaan_0517@yahoo.com>
2024-12-22 17:06:23 +0530

    Update bst_ops.c and README.md

--- Week11/README.md
+++ Week11/README.md
@@ -2,6 +2,16 @@
 
 ## bst_ops.c
 
+A program that performs the following binary tree operations:
+
+```
+1. Insert element
+2. Delete element
+3. Search for element
+4. Traverse
+5. Exit
+```
+
 Build command for linux(with GCC):
 
 ```shell
--- Week11/bst_ops.c
+++ Week11/bst_ops.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/** A node of the tree */
+/** A node of the tree
+ * @param data
+ * @param right
+ * @param left
+ */
 typedef struct node {
     /** The data in the node */
     int data;
@@ -16,6 +20,93 @@ typedef struct node {
  * @param ele The element to insert
  * @return The updated root node
  */
+node* insert(node* root, int ele);
+
+/** Finds the maximum element in a BST
+ * @param root The root of the BST
+ * @return The maximum element of the BST
+ */
+static int max(node* root);
+
+/** Deletes a node from the BST
+ * @param root The root of the BST
+ * @param ele The element to delete
+ * @param found Integer pointer storing whether the element is found
+ * @return The updated root node of the BST
+ */
+node* delete_ele(node* root, int ele, int* found);
+
+/** Searches for the entered element in BST
+ * @param root The root of the BST
+ * @param ele The element to search for
+ * @return 1 if found, 0 if not found
+ */
+int search(node* root, int ele);
+
+/** Traverses and prints the elements in the BST in sorted order
+ * @param root The root of the BST
+ */
+void traverse_inorder(node* root);
+
+int main() {
+    printf("1. Insert element\n2. Delete element\n"
+           "3. Search for element\n4. Traverse\n"
+           "5. Exit\nEnter choice: ");
+    char ch;
+    scanf(" %c", &ch);
+    node* root = NULL;
+    while (1) {
+        int ele, found = 1, error = 0;
+        switch (ch) {
+        case '1':
+            printf("Enter element to insert: ");
+            if (!scanf("%d", &ele)) {
+                int c;
+                while ((c = getchar()) != '\n' && c != EOF);
+                error = 1;
+                break;
+            }
+            root = insert(root, ele);
+            break;
+        case '2':
+            printf("Enter element to delete: ");
+            if (!scanf("%d", &ele)) {
+                int c;
+                while ((c = getchar()) != '\n' && c != EOF);
+                error = 1;
+                break;
+            }
+            root = delete_ele(root, ele, &found);
+            if (!found)
+                printf("Element not found\n");
+            break;
+        case '3':
+            printf("Enter element to search: ");
+            if (!scanf("%d", &ele)) {
+                int c;
+                while ((c = getchar()) != '\n' && c != EOF);
+                error = 1;
+                break;
+            }
+            if (search(root, ele))
+                printf("Element found\n");
+            else
+                printf("Element not found\n");
+            break;
+        case '4':
+            traverse_inorder(root);
+            printf("\n");
+            break;
+        case '5': return 0;
+        default: printf("Invalid choice\n");
+        }
+        if (error)
+            printf("Operation failed\n");
+        printf("Enter choice: ");
+        scanf(" %c", &ch);
+    }
+}
+
 node* insert(node* root, int ele) {
     if (!root) {
         root = malloc(sizeof(*root));
@@ -37,23 +128,17 @@ static int max(node* root) {
     return root -> data;
 }
 
-/** Deletes a node from the BST
- * @param root The root of the BST
- * @param ele The element to delete
- * @param found Integer pointer storing whether the element is found
- * @return The updated root node of the BST
- */
-node* delete(node* root, int ele, int* found) {
+node* delete_ele(node* root, int ele, int* found) {
     if (!root) {
         *found = 0;
         return NULL;
     }
     if (ele < root -> data) {
-        root -> left = delete(root -> left, ele, found);
+        root -> left = delete_ele(root -> left, ele, found);
         return root;
     }
     if (ele > root -> data) {
-        root -> right = delete(root -> right, ele, found);
+        root -> right = delete_ele(root -> right, ele, found);
         return root;
     }
     if (!root -> left && !root -> right) {
@@ -71,15 +156,10 @@ node* delete(node* root, int ele, int* found) {
     }
     node* temp = root -> left;
     int ele1 = root -> data = max(temp);
-    root -> left = delete(temp, ele1, found);
+    root -> left = delete_ele(temp, ele1, found);
     return root;
 }
 
-/** Searches for the entered element in BST
- * @param root The root of the BST
- * @param ele The element to search for
- * @return 1 if found, 0 if not found
- */
 int search(node* root, int ele) {
     if (!root)
         return 0;
@@ -90,69 +170,10 @@ int search(node* root, int ele) {
     return search(root -> right, ele);
 }
 
-/** Traverses and prints the elements in the BST in sorted order
- * @param root The root of the BST
- */
-void traverse(node* root) {
+void traverse_inorder(node* root) {
     if (!root)
         return;
-    traverse(root -> left);
+    traverse_inorder(root -> left);
     printf("%d ", root -> data);
-    traverse(root -> right);
-}
-
-int main() {
-    printf("1. Insert element\n2. Delete element\n"
-           "3. Search for element\n4. Traverse\n"
-           "5. Exit\nEnter choice: ");
-    char ch;
-    scanf(" %c", &ch);
-    node* root = NULL;
-    while (1) {
-        int ele, found = 1, error = 0;
-        switch (ch) {
-        case '1':
-            printf("Enter element to insert: ");
-            if (!scanf("%d", &ele)) {
-                while (getchar() != '\n');
-                error = 1;
-                break;
-            }
-            root = insert(root, ele);
-            break;
-        case '2':
-            printf("Enter element to delete: ");
-            if (!scanf("%d", &ele)) {
-                while (getchar() != '\n');
-                error = 1;
-                break;
-            }
-            delete(root, ele, &found);
-            if (!found)
-                printf("Element not found\n");
-            break;
-        case '3':
-            printf("Enter element to search: ");
-            if (!scanf("%d", &ele)) {
-                while (getchar() != '\n');
-                error = 1;
-                break;
-            }
-            if (search(root, ele))
-                printf("Element found\n");
-            else
-                printf("Element not found\n");
-            break;
-        case '4':
-            traverse(root);
-            printf("\n");
-            break;
-        case '5': return 0;
-        default: printf("Invalid choice\n");
-        }
-        if (error)
-            printf("Operation failed\n");
-        printf("Enter choice: ");
-        scanf(" %c", &ch);
-    }
+    traverse_inorder(root -> right);
 }
